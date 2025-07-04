1154e5d6146404f3b9cf86bdf8dc7df612701d89
pedromontenegromatrix <pedro.montenegro.matrix@gmail.com>
2025-07-04 14:48:34 -0500

    prueba_creacion_ec2

--- Terraform/Prueba_1/EC2/main.tf
+++ Terraform/Prueba_1/EC2/main.tf
@@ -7,6 +7,15 @@ resource "aws_vpc" "this" {
   }
 }
 
+resource "aws_subnet" "this" {
+  count             = local.borrado ? 0 : 2
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = element(local.array_nets, count.index)
+  availability_zone = element(local.aws_availability_zones, count.index)
+  depends_on        = [aws_vpc.this]
+}
+
+
 resource "aws_security_group" "this" {
   count       = local.borrado ? 0 : 1
   name        = "sgr-${var.env}-${var.project}-${var.name}-01"
@@ -30,5 +39,76 @@ resource "aws_security_group" "this" {
   tags = {
     Name = "sgr-${var.env}-${var.project}-${var.name}-01"
   }
+
+  depends_on = [aws_vpc.this]
 }
 
+
+resource "aws_iam_role" "this" {
+  count = local.borrado ? 0 : 1
+
+  name        = "role-${var.env}-${var.project}-${var.name}-01"
+  description = "Role for SSM Bastion Hosts"
+
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Principal" : {
+            "Service" : "ec2.amazonaws.com"
+          },
+          "Action" : "sts:AssumeRole"
+        }
+      ]
+  })
+
+  tags = {
+    Name = "role-${var.env}-${var.project}-${var.name}-01"
+  }
+}
+
+resource "aws_iam_instance_profile" "this" {
+  count = local.borrado ? 0 : 1
+  name  = "role${var.env}-${var.project}-${var.name}-01"
+  role  = aws_iam_role.this[0].name
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  count      = local.borrado ? 0 : 1
+  role       = aws_iam_role.this[0].name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+
+resource "aws_instance" "this" {
+  count = local.borrado ? 0 : 1
+
+  ami                    = local.bh_ami
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.this[0].id]
+  subnet_id              = aws_subnet.this[0].id
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+    instance_metadata_tags      = "enabled"
+  }
+
+  iam_instance_profile = aws_iam_instance_profile.this[0].name
+  user_data            = filebase64("scripts/bastion_host.sh")
+
+  tags = {
+    Name = "asgr-${var.env}-${var.project}-${var.name}-01"
+  }
+
+  depends_on = [
+    aws_vpc.this,
+    aws_subnet.this,
+    aws_security_group.this,
+    aws_iam_role.this,
+    aws_iam_instance_profile.this
+  ]
+}
