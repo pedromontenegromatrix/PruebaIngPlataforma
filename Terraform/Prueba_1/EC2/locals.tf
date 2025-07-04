da6420a7ad0d13624712dff0770e5b5667dd3eb9
pedromontenegromatrix <pedro.montenegro.matrix@gmail.com>
2025-07-04 16:34:32 -0500

    crear_nuevamente

--- Terraform/Prueba_1/EC2/locals.tf
+++ Terraform/Prueba_1/EC2/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  borrado                = true
+  borrado                = false
   array_nets             = ["10.0.0.0/24", "10.0.1.0/24"]
   aws_availability_zones = slice(data.aws_availability_zones.this.names, 0, length(local.array_nets))
   bh_ami                 = "ami-05ffe3c48a9991133"
