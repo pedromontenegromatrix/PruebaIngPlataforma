0565f37711636a8e97638da2d40a510156eba409
pedromontenegromatrix <pedro.montenegro.matrix@gmail.com>
2025-07-04 13:21:09 -0500

    test

--- Terraform/Prueba_1/EC2/providers.tf
+++ Terraform/Prueba_1/EC2/providers.tf
@@ -5,4 +5,12 @@ terraform {
     aws    = ">= 4.20.0"
     random = ">= 3.3.0"
   }
+
+  #backend "remote" {}
+}
+
+provider "aws" {
+  #access_key = var.access_key
+  #secret_key = var.secret_key
+  region     = var.region
 }
