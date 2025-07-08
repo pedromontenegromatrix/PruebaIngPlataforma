59931334110290ce3053b7bb285a76601f41fa69
pedromontenegromatrix <pedro.montenegro.matrix@gmail.com>
2025-07-08 11:13:27 -0500

    validar_tema_new_relic

--- Terraform/Prueba_1/EC2/new_relic.tf
+++ Terraform/Prueba_1/EC2/new_relic.tf
@@ -1,12 +1,12 @@
 resource "newrelic_alert_policy" "this" {
-  count = local.borrado ? 0 : 1
+  count = local.borrado || var.new_relic_account == "" ? 0 : 1
   name  = "Alerta EC2"
 
   depends_on = [aws_instance.this]
 }
 
 resource "newrelic_nrql_alert_condition" "this" {
-  count     = local.borrado ? 0 : 1
+  count     = local.borrado || var.new_relic_account == "" ? 0 : 1
   policy_id = newrelic_alert_policy.this[0].id
   name      = "Alerta de CPU Alta"
   type      = "static"
